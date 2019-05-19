a57f0cb8dbe2804c42e1c5a043131abccea19cb7
karenkamimura <55820kamimura@gmail.com>
2019-05-19 13:11:04 +0900

    fix toppage

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -2,11 +2,11 @@ $(function(){
     $('#vegas').vegas({
         slides: [
             { src: '/images/spain.jpg' },
-            { src: '/images/lipstick.jpg' },
+            { src: '/images/fuku.jpg' },
             { src: '/images/flower.jpg' },
             { src: '/images/girl.jpg' }
         ],
-        overlay: './images/overlays/03.png', //フォルダ『overlays』の中からオーバーレイのパターン画像を選択
+        overlay: './images/overlays/01.png', //フォルダ『overlays』の中からオーバーレイのパターン画像を選択
         transition: 'fade', //スライドを遷移させる際のアニメーション
         transitionDuration: 4000, //スライドの遷移アニメーションの時間
         delay: 10000, //スライド切り替え時の遅延時間
